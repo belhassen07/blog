c9a14d1831d22fb4694175de502409c4a9e9d655
Belhassen chelbi <chelbi.belhassen@yahoo.fr>
2020-03-10 23:36:39 +0100

    add Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,12 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: `gatsby-plugin-netlify-cms`,
+            options: {
+              enableIdentityWidget: false,
+            }
+          }
         ],
       },
     },
